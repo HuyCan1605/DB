db3ce9f780a4219d58b087cb703a5ca6045ba6d1
HuyCan1605 <95010802+HuyCan1605@users.noreply.github.com>
2023-07-20 08:29:56 +0700

    newweb

--- map.js
+++ map.js
@@ -1,16 +1,15 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDO1FpVtQNZWvfpr_S20Nb9muSAgb_mzAw",
-    authDomain: "fir-firebase-4a9b5.firebaseapp.com",
-    databaseURL: "https://fir-firebase-4a9b5-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fir-firebase-4a9b5",
-    storageBucket: "fir-firebase-4a9b5.appspot.com",
-    messagingSenderId: "358028421082",
-    appId: "1:358028421082:web:c35cd6c1e19aa0d236c2e3",
+    apiKey: "AIzaSyCpl3usEWQBmwsz2ncr8VQL5FwdRs42e_M",
+    authDomain: "module-7-realtime-db.firebaseapp.com",
+    databaseURL: "https://module-7-realtime-db-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "module-7-realtime-db",
+    storageBucket: "module-7-realtime-db.appspot.com",
+    messagingSenderId: "844850635767",
+    appId: "1:844850635767:web:347ae2d6013a28f2f5c4f2"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 let database = firebase.database();
-
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaHV5bnExNjA1IiwiYSI6ImNsazJld3E3ZzAxb2UzbG1yeDNnazliZ3kifQ.XnGNNPJab0SCBOcuDHNZ9w';
@@ -21,7 +20,12 @@ const map = new mapboxgl.Map({
     center: [105.52708575312181, 21.013430256683503],
     zoom: 13
 });
-
+map.addControl(
+    new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+);
 
 // let globalData;
 
@@ -76,7 +80,13 @@ const map = new mapboxgl.Map({
 
 
 
-map.on('load', () => {
+
+
+
+
+map.on('load', async () => {
+    const valuedb = await fetchDataFromFirebase();
+
     map.loadImage(
         'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
         (error, image) => {
@@ -84,7 +94,7 @@ map.on('load', () => {
             map.addImage('custom-marker', image);
             map.addSource('parking-car', {
                 type: 'geojson',
-                data: data
+                data: valuedb
             });
             map.addLayer({
                 'id': 'parking-car',
@@ -115,7 +125,6 @@ map.on('load', () => {
                 while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
                     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
                 }
-
                 new mapboxgl.Popup()
                     .setLngLat(coordinates)
                     .setHTML(description)
@@ -130,41 +139,84 @@ map.on('load', () => {
             map.on('mouseleave', 'parking-car', () => {
                 map.getCanvas().style.cursor = '';
             });
+
+
         });
 
+    const updateSource = setInterval(async () => {
+        const valuejson = await fetchDataFromFirebase(updateSource);
+        console.log(valuejson);
+        map.getSource('parking-car').setData(valuejson);
+    }, 2000);
+
+    async function fetchDataFromFirebase(updateSource) {
+        try {
+            const response = await fetch('https://module-7-realtime-db-default-rtdb.asia-southeast1.firebasedatabase.app/.json',
+                { method: 'GET' }
+            );
+            const datadb = await response.json();
+            const latitude = datadb.oke.place.latitude;
+            const longitude = datadb.oke.place.longitude;
+            const title = datadb.oke.title;
+            const description = datadb.oke.slot_left;
+            const latitude2 = datadb.oke2.coordinates.latitude;
+            const longitude2 = datadb.oke2.coordinates.longitude;
+            const title2 = datadb.oke2.title;
+            const description2 = datadb.oke2.slot_left;
+            return {
+                'type': 'FeatureCollection',
+                'features': [{
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [latitude2, longitude2]
+                    }, 'properties': {
+                        'title': title2,
+                        'description': '<p>Bãi đỗ xe còn ' + description2 + '</p><a href ="D:/Subject/IOC391/Firebase_test/detail.html">Click</>',
+                    }
+                }, {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [latitude, longitude]
+                    }, 'properties': {
+                        'title': title,
+                        'description': '<p>Bãi đỗ xe còn ' + description + '</p><a href ="D:/Subject/IOC391/Firebase_test/detail.html">Click</>',
+                    }
+                }]
+            };
+        } catch (err) {
+            // If the updateSource interval is defined, clear the interval to stop updating the source.
+            if (updateSource) clearInterval(updateSource);
+            throw new Error(err);
+        }
+
+    };
+
+
+    // for (const marker of geojson.features)
+
+
+
 });
+
+
+
+
+
+
+
 
 // let valuedb = firebase.database().ref('/oke');
 // valuedb.on('value', snap => {
 //     let value = snap.val();
-//     parking_car[0].description = value;
+
+//     // data.features[0].properties.description = value;
+
+
 // });
-// Create a promise for the database operation
 
 
-data = {
-    'type': 'FeatureCollection',
-    'features': [{
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [105.52708575312181, 21.013430256683503]
-        }, 'properties': {
-            'title': 'Bãi đỗ xe FPT',
-            'description': '<a href="D:/Subject/IOC391/Firebase_test/detail.html">Bãi đỗ xe còn trống </a></p>',
-            'icon': 'theatre'
-        }
-    }, {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [105.536774, 21.010295]
-        }, 'properties': {
-            'title': 'Bãi đỗ xe FPT SOFTWARE',
-            'description': '<p id = "slot2">Bãi đỗ xe không còn trống</p>',
-            'icon': 'bar'
-        }
-    }]
-};
+
 
 
